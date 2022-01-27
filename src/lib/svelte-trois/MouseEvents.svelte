5181e40e327227282b9da9f7dbd39ae83aee9016
Cam Jackson <1930451+camjackson@users.noreply.github.com>
2022-01-27 21:47:32 +1100

    Clean up handlers in MouseEvents.destroy

--- src/lib/svelte-trois/MouseEvents.svelte
+++ src/lib/svelte-trois/MouseEvents.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
+  import { onDestroy, onMount } from 'svelte';
   import type { Object3D } from 'three';
   import { getTroisInternalContext, ObjectMouseEventHandler } from './context';
   import GetChild from './GetChild.svelte';
@@ -7,12 +7,17 @@
   export let onClick: ObjectMouseEventHandler | undefined = undefined;
   export let onMouseMove: ObjectMouseEventHandler | undefined = undefined;
 
-  const three = getTroisInternalContext();
+  const trois = getTroisInternalContext();
   let child: Object3D;
 
   onMount(() => {
-    if (onClick) three.onObjectClick(child, onClick);
-    if (onMouseMove) three.onObjectMouseMove(child, onMouseMove);
+    if (onClick) trois.onObjectClick(child, onClick);
+    if (onMouseMove) trois.onObjectMouseMove(child, onMouseMove);
+  });
+
+  onDestroy(() => {
+    if (onClick) trois.removeOnObjectClick(child);
+    if (onMouseMove) trois.removeOnObjectMouseMove(child);
   });
 </script>
 
